9632e57b0e872daf44c22b3a5573e22c92691360
JedrekDabrowski <jedrzejdabrowski.jd@gmail.com>
2024-10-16 13:11:32 +0200

    remove unnecessary user agent header on android

--- src/providers/android.js
+++ src/providers/android.js
@@ -4,7 +4,6 @@ export const getAndroidVersion = async (bundleId, country) => {
   try {
     res = await fetch(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36",
         'sec-fetch-site': 'same-origin'
       }
     });
